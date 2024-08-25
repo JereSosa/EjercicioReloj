7aadc2905d6bce4c986da3fd59c421af11523b20
jeremias sosa <jere.sosa.322@gmail.com>
2024-08-25 01:02:35 -0300

    Clase reloj creada con sus metodos funcionando

--- TpLaboGitHubReloj.java
+++ TpLaboGitHubReloj.java
@@ -5,7 +5,11 @@ public class TpLaboGitHubReloj {
 
     
     public static void main(String[] args) {
-        
+        Reloj reloj = new Reloj("lunes","00:51","Rolex", 123332);
+        reloj.limpiarPantalla();
+        reloj.incrementarDia();
+        reloj.incrementarHora();
+        reloj.limpiarPantalla();
     }
     
 }
